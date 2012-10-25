63d29aa17c4bc744423ed00b780b81f5a1ef7f66
Luis R. Rodriguez <mcgrof@do-not-panic.com>
2012-10-25 10:43:51 -0700

    rel-html: relicense to AGPL
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>

--- rel-html.py
+++ rel-html.py
@@ -5,17 +5,18 @@
 
 # Copyright (C) 2012 Luis R. Rodriguez <mcgrof@do-not-panic.com>
 #
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from HTMLParser import HTMLParser
 import urllib
@@ -23,7 +24,7 @@ import ConfigParser
 import re, sys
 
 def rel_html_license():
-	return "ISC"
+	return "AGPL"
 
 def rel_html_href():
 	return '<a href="https://github.com/mcgrof/rel-html">rel-html</a>'
